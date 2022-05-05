5c1cdbefffcd403f731c641a273b20d58e33d0ad
AtomicMathO <Math.Oclock-Atome@protonmail.com>
2022-05-05 12:28:14 +0200

    Fix: rework router.js

---
+++ app/router/router.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const APIError = require("../services/APIError");
+const routerWrapper = require("../middlewares/routerWrapper");
+const handleError = require("../middlewares/handleError");
+const app = express();
+const router = express.Router();
+
+//tous les controller
+const userController = require("../controllers/userController");
+const circleController = require("../controllers/circleController");
+const chatController = require("../controllers/chatController");
+const calendarController = require("../controllers/calendarController");
+
+// Controller User
+router.post("/api/login", routerWrapper(userController.getUser));
+router.post("/api/register", routerWrapper());
+router
+  .route("/api/profil/:id(\\d+)")
+  .get(routerWrapper(userController.getUserInfo))
+  .patch(routerWrapper())
+  .delete(routerWrapper());
+router.get("/api/profil/:id/circles", routerWrapper());
+
+//Controller Circle
+router.post("/api/circle", routerWrapper());
+router.post("/api/circle/:circle_id/new/:user_id", routerWrapper());
+
+router
+  .route("/api/circle/:id")
+  .get(routerWrapper())
+  .patch(routerWrapper())
+  .delete(routerWrapper());
+
+//Controller Calendar
+router
+  .route("/api/circle/:id/calendar")
+  .get(routerWrapper())
+  .post(routerWrapper())
+  .patch(routerWrapper())
+  .delete(routerWrapper());
+
+// Controller Chat
+router.get("/api/circle/:id/chat/", routerWrapper());
+router
+  .route("/api/circle/:id/chat/:userid")
+  .post(routerWrapper())
+  .patch(routerWrapper())
+  .delete(routerWrapper());
+
+// Gestion user non authentifié - url non reconnu
+app.use(function (err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).send("Unauthorized User");
+  } else {
+    next(err);
+  }
+});
+
+//Gestion de l'erreur
+router.use(handleError);
+
+module.exports = router;
