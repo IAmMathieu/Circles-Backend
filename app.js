e6d6e7dc1bfecf9f05f0d103d6c5f37c74e6436d
AtomicMathO <Math.Oclock-Atome@protonmail.com>
2022-05-09 21:21:31 +0200

    [Fix] CORS Modification

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const router = require("./app/router/router");
 const port = process.env.PORT || 4242;
 
@@ -10,15 +11,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Cross Origin middleware -- à adapter
-app.use(function (_, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (_, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
+app.use(
+  cors({
+    allowedHeaders: ["sessionId", "Content-Type"],
+    exposedHeaders: ["sessionId"],
+    origin: "*",
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+    preflightContinue: false,
+  })
+);
 app.use(router);
 
 app.listen(port, () => {
