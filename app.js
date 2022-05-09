4a4a43dfcbfe6f4738f0f9a9e554e6f6f342d3b6
AtomicMathO <Math.Oclock-Atome@protonmail.com>
2022-05-09 21:29:00 +0200

    [Fix] CORS Modification

--- app.js
+++ app.js
@@ -22,11 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    allowedHeaders: ["sessionId", "Content-Type"],
-    exposedHeaders: ["sessionId"],
     origin: "*",
-    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
-    preflightContinue: false,
   })
 );
 app.use(router);
