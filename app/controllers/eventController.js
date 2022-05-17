5aae60e884a9ee4ff9b374078af61c701a01305d
robinho54 <poppelrobin@gmail.com>
2022-05-17 09:48:19 +0200

    feature : sanitize finish

--- app/controllers/eventController.js
+++ app/controllers/eventController.js
@@ -1,48 +1,51 @@
-const eventDatamapper = require("../datamapper/eventDatamapper");
+const sanitizeHtml  =  require ('sanitize-html') ;
+const eventDataMapper = require("../datamapper/eventDatamapper");
 
 const eventController = {
   async addEvent(req, res) {
-    const circle = req.body;
-    circle.circle_id = req.params.circle_id;
-    const addEvent = await eventDatamapper.addEvent(circle);
+    const userId = req.params.id;
+    req.body.title = sanitizeHtml(req.body.title);
+    req.body.description = sanitizeHtml(req.body.description);
+    req.body.color = sanitizeHtml(req.body.color);
+    const addEvent = await eventDataMapper.addEvent(req.body, userId);
     res.json(addEvent);
   },
 
   async patchEvent(req, res) {
-    const circleId = req.params.circle_id;
-    req.body.event_id = req.params.event_id;
-    console.log(req.body);
+    const userId = req.params.id;
+    const eventId = req.params.event_id;
 
-    const patchEvent = await eventDatamapper.patchEvent(req.body, circleId);
+    req.body.title = sanitizeHtml(req.body.title);
+    req.body.description = sanitizeHtml(req.body.description);
+    req.body.color = sanitizeHtml(req.body.color);
+
+    const patchEvent = await eventDataMapper.patchEvent(
+      req.body,
+      userId,
+      eventId
+    );
     res.json(patchEvent);
   },
 
   async allEvent(req, res) {
-    const circleId = req.params.id;
-    const allEvent = await eventDatamapper.allEvent(circleId);
+    const circleId = req.params.circle_id;
+    const allEvent = await eventDataMapper.allEvent(circleId);
     res.json(allEvent);
   },
 
   async oneEvent(req, res) {
     const eventId = req.params.event_id;
-    const circleId = req.params.circle_id;
-    const oneEvent = await eventDatamapper.oneEvent(eventId, circleId);
+    const oneEvent = await eventDataMapper.oneEvent(eventId);
     res.json(oneEvent);
   },
 
   async deleteEvent(req, res) {
     const eventId = req.params.event_id;
-    const circleId = req.params.circle_id;
-    const userId = req.body.user_id;
-    const deleteEvent = await eventDatamapper.deleteEvent(
-      eventId,
-      circleId,
-      userId
-    );
+    const deleteEvent = await eventDataMapper.deleteEvent(eventId, req.body);
     if (deleteEvent) {
-      res.status(200).send("Event is deleted");
+      res.send("Event is deleted");
     } else {
-      res.status(400).send("Error, event can't be deleted ");
+      res.send("Error, event can't be deleted ");
     }
   },
 };
