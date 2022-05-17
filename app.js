eac0c68bb75c1d9ab916b02cb3ae7a404ca9470d
AtomicMathO <Math.Oclock-Atome@protonmail.com>
2022-05-17 10:29:25 +0200

    [Fix] changed cors policies

--- app.js
+++ app.js
@@ -17,12 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: [
-    "*",
-    "91.86.45.85",
-    "176.159.219.162",
-    "https://apocircles.surge.sh",
-  ],
+  origin: "*, 91.86.45.85, 176.159.219.162, https://apocircles.surge.sh",
   methods: ["GET", "HEAD", "POST", "PATCH", "DELETE", "OPTIONS"],
   credentials: true,
   preflightContinue: false,
