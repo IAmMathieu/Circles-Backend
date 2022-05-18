51f5c5e14755b9b265c299d1872b522c872d6543
AtomicMathO <Math.Oclock-Atome@protonmail.com>
2022-05-18 16:44:56 +0200

    [Feat] Started to work on circle invite

--- app/services/mailer.js
+++ app/services/mailer.js
@@ -1,28 +1,49 @@
 const nodemailer = require("nodemailer");
 
-exports = module.exports = function sendEmailValidator(email, validationCode) {
-  let Transport = nodemailer.createTransport({
+const sendMail = {
+  Transport: nodemailer.createTransport({
     service: "Gmail",
     auth: {
       user: process.env.AMAILADDR,
       pass: process.env.AMAILPASS,
     },
-  });
+  }),
 
-  let mailOptions;
-  let sender = "Circles Admin";
-  mailOptions = {
-    from: sender,
-    to: email,
-    subject: "Confirmation de votre adresse mail",
-    html: `Veuillez suivre ce <a href=http://localhost:4242/verify/${validationCode}> lien </a> confirmer votre adresse mail. Merci`,
-  };
+  sendEmailValidator(email, validationCode) {
+    let mailOptions;
+    let sender = "Circles Admin";
+    mailOptions = {
+      from: sender,
+      to: email,
+      subject: "Confirmation de votre adresse mail",
+      html: `Veuillez suivre ce <a href=http://localhost:4242/verify/${validationCode}> lien </a> confirmer votre adresse mail. Merci`,
+    };
 
-  Transport.sendMail(mailOptions, (err, res) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Message envoyé");
-    }
-  });
+    Transport.sendMail(mailOptions, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Message envoyé");
+      }
+    });
+  },
+
+  sendCircleInvite(email, circleCode) {
+    let mailOptions;
+    let sender = "Circles Admin";
+    mailOptions = {
+      from: sender,
+      to: email,
+      subject: "Vous avez été invité à rejoindre une communauté sur Circles",
+      html: `Veuillez suivre ce <a href=http://localhost:4242/invite/${circleCode}> lien </a> confirmer votre adresse mail. Merci`,
+    };
+
+    Transport.sendMail(mailOptions, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Message envoyé");
+      }
+    });
+  },
 };
